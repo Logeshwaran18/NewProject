dae73ed2b46688062d7c98996414da5a30eff76c
Logeshwaran18 <logeshwaran199718@gmail.com>
2021-05-21 11:35:45 +0530

    hai

--- src/test/java/org/test/First.java
+++ src/test/java/org/test/First.java
@@ -5,7 +5,14 @@ import org.testng.annotations.Test;
 public class First {
 	@Test
 	private void tc1() {
-		System.out.println("happy");
+		System.out.println("jjjjjjjjjjj");
+		String a="Welcome to java";
+		String[] sp = a.split(" ");
+		String rev="";
+		for (int i = sp.length; i >=0; i--) {
+			rev=rev+sp[i]+" ";
+		}
+		System.out.println(rev);
 
 	}
 
