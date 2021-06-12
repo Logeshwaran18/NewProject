10a2c9b8600a3488937680b464b409280a08135c
Logeshwaran18 <logeshwaran199718@gmail.com>
2021-06-12 23:00:58 +0530

    fibbno

---
+++ src/test/java/org/test/Fibbno.java
@@ -0,0 +1,18 @@
+package org.test;
+
+public class Fibbno {
+
+	public static void main(String[] args) {
+		int a=0;
+		int b=1;
+		for (int i = 0; i < 10; i++) {
+			int c=a+b;
+			System.out.println(c);
+			a=b;
+			b=c;
+			
+		}
+		
+		
+	}
+}
